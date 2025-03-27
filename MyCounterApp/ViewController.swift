6f53d28d7208bc2cd1da528f76d6bc087d0dd26e
hyemin <hmpark15@gmail.com>
2025-03-27 12:53:09 +0900

    Feat: Add numeric display label
    Implement UI element to show updated numerical values

--- MyCounterApp/ViewController.swift
+++ MyCounterApp/ViewController.swift
@@ -1,19 +1,35 @@
-//
-//  ViewController.swift
-//  MyCounterApp
-//
-//  Created by 박혜민 on 3/27/25.
-//
-
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    let label = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // view가 load 되었을 때 configureUI 함수 실행
+        configureUI()
     }
-
-
+    
+    private func configureUI() {
+        // UILabel 속성
+        view.backgroundColor = .black
+        label.textColor = .white
+        label.text = "12345"
+        label.textAlignment = .right
+        label.font = .boldSystemFont(ofSize: 60)
+        
+        // view에 노출
+        [label]
+            .forEach { view.addSubview($0) }
+        
+        // autolayout 설정
+        label.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(30)
+            $0.trailing.equalToSuperview().offset(-30)
+            $0.top.equalToSuperview().offset(200)
+            $0.height.equalTo(100)
+            
+        }
+    }
 }
-
